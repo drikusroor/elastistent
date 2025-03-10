d099221d94cebf4b4ae2a652b31906e5d0e20efe
Drikus Roor <drikusroor@gmail.com>
2025-03-10 09:51:33 +0100

    feat: Enhance TeethModel3D stories with customizable camera and object positioning

--- src/stories/TeethModel3D.stories.tsx
+++ src/stories/TeethModel3D.stories.tsx
@@ -14,7 +14,10 @@ const teethModelMeta = {
   },
   args: {
     onToothClick: fn(),
-
+    camZoom: 3.5,
+    camPosition: [0, 0, 5],
+    camFov: 50,
+    objPosition: [0, 1.33, 0],
   },
 } satisfies Meta<typeof TeethModel3D>;
 
@@ -24,27 +27,34 @@ type TeethModelStory = StoryObj<typeof teethModelMeta>;
 
 // For 3D components, we need to wrap them in a Canvas
 export const DefaultTeethModel: TeethModelStory = {
-  render: (args) => (
-    <div style={{ width: "600px", height: "400px" }}>
-      <Canvas camera={{ position: [0, 0, 5], fov: 50, zoom: 2 }}> 
-        <ambientLight intensity={2.5} />
-        <pointLight position={[10, 10, 10]} />
-        <TeethModel3D {...args} position={[0, 0, 0]} />
-      </Canvas>
-    </div>
-  ),
+  render: (args) => {
+    const { camZoom, camPosition, camFov, objPosition } = args;
+
+    return (
+      <div style={{ width: "600px", height: "400px" }}>
+        <Canvas camera={{ position: camPosition, fov: camFov, zoom: camZoom }}>
+          <ambientLight intensity={2.5} />
+          <pointLight position={[10, 10, 10]} />
+          <TeethModel3D {...args} position={objPosition} />
+        </Canvas>
+      </div>
+    );
+  },
 };
 
 export const DewaTeethModel: TeethModelStory = {
-  render: (args) => (
-    <div style={{ width: "600px", height: "400px" }}>
-      <Canvas camera={{ position: [0, 0, 50], fov: 50 }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <TeethModel3DDewa {...args} />
-      </Canvas>
-    </div>
-  ),
+  render: (args) => {
+
+    return (
+      <div style={{ width: "600px", height: "400px" }}>
+        <Canvas camera={{ position: [0, 0, 50], fov: 50, zoom: 20 }}>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} />
+          <TeethModel3DDewa {...args} position={[0, -20, 0]} />
+        </Canvas>
+      </div>
+    );
+  },
 };
 
 export const WithClickHandler: TeethModelStory = {
